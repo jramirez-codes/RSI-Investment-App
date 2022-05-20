191ed926901a8b6357e80ccef90392a3b320291a
jordan-s-ramirez <jordan.s.ramirez.com>
2022-05-20 17:01:38 -0400

    Implemented searching

--- components/rsiStockSearch.js
+++ components/rsiStockSearch.js
@@ -16,6 +16,23 @@ const chartConfig = {
   useShadowColorFromDataset: false,
 };
 
+async function getSearchData(keywords) {
+  var url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+keywords+"&apikey=ZKAWVVN5WJRWFHD2"
+  var data = await fetch(url)
+  .then((response) => response.json())
+  .then((json) => {
+    return json;
+  })
+  .then(data => {
+    return data;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+  return data;
+}
+
 // Get RSI data from alppha vantage
 async function getData(stock) {
   var url = "https://www.alphavantage.co/query?function=RSI&symbol="+stock+"&interval=daily&time_period=10&series_type=open&apikey=ZKAWVVN5WJRWFHD2"
@@ -34,18 +51,24 @@ async function getData(stock) {
   return data;
 }
 
+// When Text input is changed
+async function onTextInput(input) {
+  return await getSearchData(input)
+}
+
 // When Button is clicked
 async function onPress(stock) {
   if(stock === "") {
     console.log("No stock selected");
-    return "NO Stock Selected!";
+    return "No Stock Selected!";
   }
-  console.log("Pressed!");
+  console.log("Getting Data for " + stock);
   var data = await getData(stock);
+  console.log(data);
   return data;
 }
 
-async function augmentData(data) {
+async function augmentDataRSI(data) {
   // Variables
   var metaJson = data["Meta Data"];
   var rsiDataJson = data["Technical Analysis: RSI"];
@@ -65,8 +88,6 @@ async function augmentData(data) {
     rsiData.push(parseFloat(rsiNumb));
     rsiLabels.push(i)
   }
-  // console.log(rsiData);
-  // console.log(rsiLabels);
 
   // NOTE: I SET A HARD SET LIMIT TO 10 Entries for RSI DATA
   rsiData = rsiData.slice(0,10).reverse()
@@ -75,17 +96,55 @@ async function augmentData(data) {
   return [rsiLabels, rsiData, metaData]
 }
 
+async function augmentDataSearch(data) {
+  var searchData = []
+  for(var x = 0; x < data.length; x++) {
+    searchData.push([data[x]["1. symbol"], data[x]["2. name"]])
+  }
+  // Return array of tuples with symbol and name
+  return searchData.slice(0,3);
+}
+
 function StockSearch() {
   const [inputStock, setInputStock] = useState("");
-  const [chartData, setChartData] = useState(0);
+  const [stockSearch, setStockSearch] = useState(null)
+  const [chartData, setChartData] = useState(null);
 
-  const onButtonPress = async() => {
-    // Update data and stock input
-    var rsiData = await onPress(inputStock)
+  // When text is inputed into textInput
+  const onTextSearch = async(searchText) => {
+    // Set text to type text
+    setInputStock(searchText);
+    
+    // Check if length is greater than 3 character
+    if(searchText.length === 5 || searchText.length === 8) {
+      var searchData = await onTextInput(searchText)
+      searchData = await augmentDataSearch(searchData["bestMatches"])
+      console.log(searchData)
+      setStockSearch(searchData)
+    }
+    else if(searchText.length < 5) {
+      setStockSearch(null)
+    }
+  } 
+
+  // When "Get RSI" Button is clicked
+  const onButtonPress = async(stock) => {
+    var rsiData
+    if(stock !== null) {
+      // Update data and stock input if submitted
+      rsiData = await onPress(stock)
+      // Update searchs back to 0
+      setStockSearch(null)
+    }else {
+      rsiData = await onPress(inputStock)
+    } 
+
     if(Object.keys(rsiData).length === 0) {
       return
     }
-    rsiData = await augmentData(rsiData)
+    // Transform data into useable form
+    rsiData = await augmentDataRSI(rsiData)
+    console.log(rsiData)
     await setChartData({
       labels: rsiData[0],
       datasets: [
@@ -99,18 +158,40 @@ function StockSearch() {
     })
   }
 
+  // Display Search Data for the user
+  const DisplaySearchData = (props) => {
+    if(props.data === null) {
+      return(<View>{null}</View>);
+    }
+  
+    var data = props.data
+    var key = 0;
+    const searchedData = data.map((stock) =>(
+        <Button key={key++} style={[{alignSelf:"center"}]} mode="text" onPress={() => onButtonPress(stock[0])}>
+          {stock[0] + " - " + stock[1]}
+        </Button>
+    ));
+  
+    return (
+      <View style={[{justifyContent:'space-between', marginBottom: 10}]}>
+        {searchedData}
+      </View>
+    );
+  }
+
   return(
     <>
     <View style={[{justifyContent:'space-between', marginBottom: 10}]}>
-      <TextInput label="TINKER Stock" value={inputStock} onChangeText={inputStock => setInputStock(inputStock)} />
+      <TextInput label="TINKER Stock" value={inputStock} onChangeText={inputStock => onTextSearch(inputStock)} />
     </View>
+    <DisplaySearchData data={stockSearch}/>
     <View style={[{justifyContent:'space-between', marginBottom: 10}]}>
-      <Button style={[{ width: "60%", alignSelf:"center"}]} mode="contained" onPress={() => onButtonPress()}>
+      <Button style={[{ width: "60%", alignSelf:"center"}]} mode="contained" onPress={() => onButtonPress(null)}>
         Get RSI
       </Button>
     </View>
     <View style={[{marginBottom: 10, borderRadius: 30, backgroundColor: "black", width:"90%", alignSelf:"center"}]}>
-      {chartData === 0 ? (null): (<LineChart data={chartData} width={screenWidth*0.9} height={250} chartConfig={chartConfig}  verticalLabelRotation={15} bezier/>)}
+      {chartData === null ? (null): (<LineChart data={chartData} width={screenWidth*0.9} height={250} chartConfig={chartConfig}  verticalLabelRotation={15} bezier/>)}
     </View>
     </>
   );
